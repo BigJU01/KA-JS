9757bfed4ff61a378cbae3a2d8d70fa3e5f08c16
BigJU01 <47334779+BigJU01@users.noreply.github.com>
2019-04-08 00:14:24 -0400

    Create Dinner.js
    "Project: What's for Dinner?"

---
+++ Dinner.js
@@ -0,0 +1,27 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300);
+fill(71, 33, 33);
+noStroke();
+//burger
+ellipse(162, 130, 130, 130);
+stroke(8, 8, 8);
+strokeWeight(3);
+line(148, 165, 115, 134);
+line(170, 166, 115, 114);
+line(193, 162, 120, 92);
+line(212, 150, 147, 87);
+line(212, 121, 167, 83);
+//cheese
+fill(247, 255, 0);
+rect(82, 218, 101, 93);
+//ketchup
+fill(255, 0, 0);
+ellipse(133, 265, 50, 50);
+//bun
+fill(189, 151, 36);
+ellipse(243, 188, 120, 120);
+stroke(232, 208, 208);
+strokeWeight(3);
+line(248, 195, 223, 170);
+line(223, 197, 247, 166);
